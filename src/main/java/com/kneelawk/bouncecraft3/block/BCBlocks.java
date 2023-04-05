91e918a975a44910a388d8f7ef2f820ea428a6e6
Kneelawk <kneelawk@gmail.com>
2023-04-05 12:24:07 -0700

    Finish bouncium blocks

--- src/main/java/com/kneelawk/bouncecraft3/block/BCBlocks.java
+++ src/main/java/com/kneelawk/bouncecraft3/block/BCBlocks.java
@@ -1,10 +1,12 @@
 package com.kneelawk.bouncecraft3.block;
 
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
 import net.minecraft.block.Material;
+import net.minecraft.item.BlockItem;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.DyeColor;
@@ -42,25 +44,30 @@ public class BCBlocks {
         final FabricBlockSettings bounciumBlocks =
             FabricBlockSettings.of(Material.STONE).requiresTool().strength(1.5f, 6.0f);
 
-        WHITE_BOUNCIUM_BLOCK = register(id("white_bouncium_block"), new Block(copyOf(bounciumBlocks).mapColor(DyeColor.WHITE)));
-        ORANGE_BOUNCIUM_BLOCK = register(id("orange_bouncium_block"), new Block(copyOf(bounciumBlocks).mapColor(DyeColor.ORANGE)));
-        MAGENTA_BOUNCIUM_BLOCK = register(id("magenta_bouncium_block"), new Block(copyOf(bounciumBlocks).mapColor(DyeColor.MAGENTA)));
-        LIGHT_BLUE_BOUNCIUM_BLOCK = register(id("light_blue_bouncium_block"), new Block(copyOf(bounciumBlocks).mapColor(DyeColor.LIGHT_BLUE)));
-        YELLOW_BOUNCIUM_BLOCK = register(id("yellow_bouncium_block"), new Block(copyOf(bounciumBlocks).mapColor(DyeColor.YELLOW)));
-        LIME_BOUNCIUM_BLOCK = register(id("lime_bouncium_block"), new Block(copyOf(bounciumBlocks).mapColor(DyeColor.LIME)));
-        PINK_BOUNCIUM_BLOCK = register(id("pink_bouncium_block"), new Block(copyOf(bounciumBlocks).mapColor(DyeColor.PINK)));
-        GRAY_BOUNCIUM_BLOCK = register(id("gray_bouncium_block"), new Block(copyOf(bounciumBlocks).mapColor(DyeColor.GRAY)));
-        LIGHT_GRAY_BOUNCIUM_BLOCK = register(id("light_gray_bouncium_block"), new Block(copyOf(bounciumBlocks).mapColor(DyeColor.LIGHT_GRAY)));
-        CYAN_BOUNCIUM_BLOCK = register(id("cyan_bouncium_block"), new Block(copyOf(bounciumBlocks).mapColor(DyeColor.CYAN)));
-        PURPLE_BOUNCIUM_BLOCK = register(id("purple_bouncium_block"), new Block(copyOf(bounciumBlocks).mapColor(DyeColor.PURPLE)));
-        BLUE_BOUNCIUM_BLOCK = register(id("blue_bouncium_block"), new Block(copyOf(bounciumBlocks).mapColor(DyeColor.BLUE)));
-        BROWN_BOUNCIUM_BLOCK = register(id("brown_bouncium_block"), new Block(copyOf(bounciumBlocks).mapColor(DyeColor.BROWN)));
-        GREEN_BOUNCIUM_BLOCK = register(id("green_bouncium_block"), new Block(copyOf(bounciumBlocks).mapColor(DyeColor.GREEN)));
-        RED_BOUNCIUM_BLOCK = register(id("red_bouncium_block"), new Block(copyOf(bounciumBlocks).mapColor(DyeColor.RED)));
-        BLACK_BOUNCIUM_BLOCK = register(id("black_bouncium_block"), new Block(copyOf(bounciumBlocks).mapColor(DyeColor.BLACK)));
+        WHITE_BOUNCIUM_BLOCK = registerWithItem(id("white_bouncium_block"), new Block(copyOf(bounciumBlocks).mapColor(DyeColor.WHITE)));
+        ORANGE_BOUNCIUM_BLOCK = registerWithItem(id("orange_bouncium_block"), new Block(copyOf(bounciumBlocks).mapColor(DyeColor.ORANGE)));
+        MAGENTA_BOUNCIUM_BLOCK = registerWithItem(id("magenta_bouncium_block"), new Block(copyOf(bounciumBlocks).mapColor(DyeColor.MAGENTA)));
+        LIGHT_BLUE_BOUNCIUM_BLOCK = registerWithItem(id("light_blue_bouncium_block"), new Block(copyOf(bounciumBlocks).mapColor(DyeColor.LIGHT_BLUE)));
+        YELLOW_BOUNCIUM_BLOCK = registerWithItem(id("yellow_bouncium_block"), new Block(copyOf(bounciumBlocks).mapColor(DyeColor.YELLOW)));
+        LIME_BOUNCIUM_BLOCK = registerWithItem(id("lime_bouncium_block"), new Block(copyOf(bounciumBlocks).mapColor(DyeColor.LIME)));
+        PINK_BOUNCIUM_BLOCK = registerWithItem(id("pink_bouncium_block"), new Block(copyOf(bounciumBlocks).mapColor(DyeColor.PINK)));
+        GRAY_BOUNCIUM_BLOCK = registerWithItem(id("gray_bouncium_block"), new Block(copyOf(bounciumBlocks).mapColor(DyeColor.GRAY)));
+        LIGHT_GRAY_BOUNCIUM_BLOCK = registerWithItem(id("light_gray_bouncium_block"), new Block(copyOf(bounciumBlocks).mapColor(DyeColor.LIGHT_GRAY)));
+        CYAN_BOUNCIUM_BLOCK = registerWithItem(id("cyan_bouncium_block"), new Block(copyOf(bounciumBlocks).mapColor(DyeColor.CYAN)));
+        PURPLE_BOUNCIUM_BLOCK = registerWithItem(id("purple_bouncium_block"), new Block(copyOf(bounciumBlocks).mapColor(DyeColor.PURPLE)));
+        BLUE_BOUNCIUM_BLOCK = registerWithItem(id("blue_bouncium_block"), new Block(copyOf(bounciumBlocks).mapColor(DyeColor.BLUE)));
+        BROWN_BOUNCIUM_BLOCK = registerWithItem(id("brown_bouncium_block"), new Block(copyOf(bounciumBlocks).mapColor(DyeColor.BROWN)));
+        GREEN_BOUNCIUM_BLOCK = registerWithItem(id("green_bouncium_block"), new Block(copyOf(bounciumBlocks).mapColor(DyeColor.GREEN)));
+        RED_BOUNCIUM_BLOCK = registerWithItem(id("red_bouncium_block"), new Block(copyOf(bounciumBlocks).mapColor(DyeColor.RED)));
+        BLACK_BOUNCIUM_BLOCK = registerWithItem(id("black_bouncium_block"), new Block(copyOf(bounciumBlocks).mapColor(DyeColor.BLACK)));
     }
 
     private static Block register(Identifier id, Block block) {
+        return Registry.register(Registries.BLOCK, id, block);
+    }
+
+    private static Block registerWithItem(Identifier id, Block block) {
+        Registry.register(Registries.ITEM, id, new BlockItem(block, new FabricItemSettings()));
         return Registry.register(Registries.BLOCK, id, block);
     }
 }
--- src/main/java/com/kneelawk/bouncecraft3/misc/BCCreativeTabs.java
+++ src/main/java/com/kneelawk/bouncecraft3/misc/BCCreativeTabs.java
@@ -4,6 +4,7 @@ import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
 
 import net.minecraft.item.ItemGroup;
 
+import com.kneelawk.bouncecraft3.block.BCBlocks;
 import com.kneelawk.bouncecraft3.item.BCItems;
 
 import static com.kneelawk.bouncecraft3.Constants.id;
@@ -14,6 +15,23 @@ public class BCCreativeTabs {
     public static void init() {
         BOUNCE_CRAFT = FabricItemGroup.builder(id("bounce_craft")).entries((ctx, entries) -> {
             entries.add(BCItems.BOUNCIUM_BUCKET);
+
+            entries.add(BCBlocks.WHITE_BOUNCIUM_BLOCK);
+            entries.add(BCBlocks.ORANGE_BOUNCIUM_BLOCK);
+            entries.add(BCBlocks.MAGENTA_BOUNCIUM_BLOCK);
+            entries.add(BCBlocks.LIGHT_BLUE_BOUNCIUM_BLOCK);
+            entries.add(BCBlocks.YELLOW_BOUNCIUM_BLOCK);
+            entries.add(BCBlocks.LIME_BOUNCIUM_BLOCK);
+            entries.add(BCBlocks.PINK_BOUNCIUM_BLOCK);
+            entries.add(BCBlocks.GRAY_BOUNCIUM_BLOCK);
+            entries.add(BCBlocks.LIGHT_GRAY_BOUNCIUM_BLOCK);
+            entries.add(BCBlocks.CYAN_BOUNCIUM_BLOCK);
+            entries.add(BCBlocks.PURPLE_BOUNCIUM_BLOCK);
+            entries.add(BCBlocks.BLUE_BOUNCIUM_BLOCK);
+            entries.add(BCBlocks.BROWN_BOUNCIUM_BLOCK);
+            entries.add(BCBlocks.GREEN_BOUNCIUM_BLOCK);
+            entries.add(BCBlocks.RED_BOUNCIUM_BLOCK);
+            entries.add(BCBlocks.BLACK_BOUNCIUM_BLOCK);
         }).build();
     }
 }
